562f455b82389ae3d8f9a91a32e0391486c4c653
Hien Nguyen <mcjambi@gmail.com>
2022-08-16 18:35:10 +0700

    update
    update

--- release.sh
+++ release.sh
@@ -3,8 +3,10 @@ set -e
 
 rm -rf ./dist;
 ./build_android.sh;
-./build_ios.sh;
+# ./build_ios.sh;
 
 cd ./dist;
 
 tar -czvf ../release.tar.gz ./
+#remove all in docker, clear cache and image and container
+#docker system prune -a
